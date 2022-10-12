39f12a6e9f4088cd9ed55dd510dee7afc63173a7
anushkashs <115626569+anushkashs@users.noreply.github.com>
2022-10-12 15:48:31 +0530

    Registering data models to database
    This will add the all the data models to database with admin as user

--- fithit/admin.py
+++ fithit/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
-from .models import video_content
+from .models import video_content,Eat_better
+# Register your models here
 
 admin.site.register(video_content),
-
-
+admin.site.register(Eat_better)
