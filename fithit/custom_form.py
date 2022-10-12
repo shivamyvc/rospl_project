c738b969be85f1108a672fc2ded1c9bd36f667da
odrinrodrigues <88288233+odrinrodrigues@users.noreply.github.com>
2022-10-12 16:09:34 +0530

    Update custom_form.py
    AUthentication Using Django authentication

--- fithit/custom_form.py
+++ fithit/custom_form.py
@@ -1,8 +1,9 @@
 from django import forms
 from django.forms import fields, widgets
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
+
 # Create your tests here.
 
 ###Form Validators
@@ -23,3 +24,13 @@ class signupform(UserCreationForm):
         self.fields['password1'].widget.attrs.update({'placeholder':_('Password')})        
         self.fields['password2'].widget.attrs.update({'placeholder':_('Repeat password')})
    
+### AUthentication Using Django authentication
+
+class loginform(AuthenticationForm):
+
+    def _init_(self, *args, **kwargs):
+        super()._init_(*args, **kwargs)
+        
+        self.fields['username'].widget.attrs.update({'placeholder':_('Username')})
+        
+        self.fields['password'].widget.attrs.update({'placeholder':_('Password')})
